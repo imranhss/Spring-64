67edfaa5ea60b4a15ff483f60951161176095267
Muhammad Emran Hossain <emranhss@gmail.com>
2025-07-16 18:25:51 +0600

    add

--- project/src/main/java/com/emranhss/project/entity/District.java
+++ project/src/main/java/com/emranhss/project/entity/District.java
@@ -22,6 +22,9 @@ public class District {
     @JsonIgnore
     private List<PoliceStation> policeStations;
 
+    @ManyToOne
+    @JoinColumn(name = "division_id")
+    private Division division;
 
     public District() {
     }
@@ -58,5 +61,13 @@ public class District {
 
     public void setPoliceStations(List<PoliceStation> policeStations) {
         this.policeStations = policeStations;
+    }
+
+    public Division getDivision() {
+        return division;
+    }
+
+    public void setDivision(Division division) {
+        this.division = division;
     }
 }
