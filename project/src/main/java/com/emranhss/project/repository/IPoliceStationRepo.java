d4cf823e7fe22e4d4ad16b861a58af880d32fca1
Muhammad Emran Hossain <emranhss@gmail.com>
2025-07-16 18:48:56 +0600

    add

--- project/src/main/java/com/emranhss/project/repository/IPoliceStationRepo.java
+++ project/src/main/java/com/emranhss/project/repository/IPoliceStationRepo.java
@@ -4,7 +4,11 @@ import com.emranhss.project.entity.PoliceStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IPoliceStationRepo extends JpaRepository<PoliceStation, Integer> {
+
+    List<PoliceStation> findByDistrictId(Integer districtId);
 
 }
--- project/src/main/java/com/emranhss/project/service/PoliceStationService.java
+++ project/src/main/java/com/emranhss/project/service/PoliceStationService.java
@@ -1,6 +1,8 @@
 package com.emranhss.project.service;
 
+import com.emranhss.project.entity.District;
 import com.emranhss.project.entity.PoliceStation;
+import com.emranhss.project.repository.IDistrictRepo;
 import com.emranhss.project.repository.IPoliceStationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,9 @@ public class PoliceStationService {
 
     @Autowired
     private IPoliceStationRepo policeStationRepo;
+
+    @Autowired
+    private IDistrictRepo districtRepository;
 
 
     public void saveOrUpdate(PoliceStation ps) {
@@ -33,4 +38,18 @@ public class PoliceStationService {
         policeStationRepo.deleteById(id);
     }
 
+    public List<PoliceStation> findByDistrict(Integer districtId) {
+        return policeStationRepo.findByDistrictId(districtId);
+    }
+
+    public PoliceStation assignToDistrict(Integer policeStationId, Integer districtId) {
+        PoliceStation ps = policeStationRepo.findById(policeStationId)
+                .orElseThrow(() -> new RuntimeException("PoliceStation not found"));
+
+        District district = districtRepository.findById(districtId)
+                .orElseThrow(() -> new RuntimeException("District not found"));
+
+        ps.setDistrict(district);
+        return policeStationRepo.save(ps);
+    }
 }
