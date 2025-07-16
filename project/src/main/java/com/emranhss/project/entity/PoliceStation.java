797851c15e3e0e4b11715a2da2ddf883ca00efb4
Muhammad Emran Hossain <emranhss@gmail.com>
2025-07-16 17:10:22 +0600

    add

--- project/src/main/java/com/emranhss/project/entity/PoliceStation.java
+++ project/src/main/java/com/emranhss/project/entity/PoliceStation.java
@@ -1,5 +1,7 @@
 package com.emranhss.project.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -13,10 +15,32 @@ public class PoliceStation {
     @Column(length = 50, nullable = false)
     private String name;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "district_id")
+    @JsonIgnore
+    private District district;
+
     public PoliceStation() {
     }
 
-    public PoliceStation(int id, String name) {}
+    public PoliceStation(int id, String name, District district) {
+        this.id = id;
+        this.name = name;
+        this.district = district;
+    }
+
+    public PoliceStation(String name, District district) {
+        this.name = name;
+        this.district = district;
+    }
+
+    public PoliceStation(String name) {
+        this.name = name;
+    }
+
+    public PoliceStation(District district) {
+        this.district = district;
+    }
 
     public int getId() {
         return id;
@@ -32,5 +56,13 @@ public class PoliceStation {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
     }
 }
---
+++ project/src/main/java/com/emranhss/project/repository/IDistrictRepo.java
@@ -0,0 +1,14 @@
+package com.emranhss.project.repository;
+
+import com.emranhss.project.entity.District;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IDistrictRepo extends JpaRepository<District, Integer> {
+
+    public District findByName(String name);
+
+}
