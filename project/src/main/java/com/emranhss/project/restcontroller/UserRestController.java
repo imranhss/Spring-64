42b788f0f01e66df63bf5a0bf2e5e44eec1b43aa
Muhammad Emran Hossain <emranhss@gmail.com>
2025-07-17 18:45:17 +0600

    add

---
+++ project/src/main/java/com/emranhss/project/restcontroller/UserRestController.java
@@ -0,0 +1,34 @@
+package com.emranhss.project.restcontroller;
+
+import com.emranhss.project.service.UserService;
+import com.emranhss.project.entity.User;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user/")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<String> saveOrUpdate(@RequestBody User user) {
+        try {
+            userService.saveOrUpdate(user);
+            return ResponseEntity.ok("Data Saved");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+
+
+
+}
